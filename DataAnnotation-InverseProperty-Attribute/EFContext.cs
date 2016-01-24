a947ad8b9fa5d0f212b03f76c108d68023312f7f
TekTutorialsHub <admin@TekTutorialsHub.com>
2016-01-24 11:25:30 +0530

    Created

---
+++ DataAnnotation-InverseProperty-Attribute/EFContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DataAnnotation_InverseProperty_Attribute
+{
+    public class EFContext : DbContext
+    {
+        public EFContext()
+            : base("EFDatabase")
+        {
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseAlways<EFContext>());
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+
+
+    }
+
+
+}
+
---
+++ DataAnnotation-InverseProperty-Attribute/models.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAnnotation_InverseProperty_Attribute
+{
+
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+
+        //Navigational property
+        public Department Department { get; set; }
+    }
+
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+
+        //Navigational property
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+
+
+    public class Flight
+    {
+        public int FlightID { get; set; }
+        public string Name { get; set; }
+
+        
+        public Airport DepartureAirport { get; set; }
+
+     
+        public Airport ArrivalAirport { get; set; }
+    }
+
+
+    public class Airport
+    {
+        public int AirportID { get; set; }
+        public string Name { get; set; }
+        [InverseProperty("DepartureAirport")]
+        public virtual ICollection<Flight> DepartingFlights { get; set; }
+        [InverseProperty("ArrivalAirport")]
+        public virtual ICollection<Flight> ArrivingFlights { get; set; }
+    }
+
+
+}
